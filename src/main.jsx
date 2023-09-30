d6f277774e6709aa35a19fd6fc8677d03a445394
duekalion <aycodergymer21@gmail.com>
2023-09-30 20:08:41 +0100

    Added my packgages

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React, {createContext} from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div>
+    Groceries Store
+  </div>
+)
